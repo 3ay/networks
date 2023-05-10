a1ba289b97f023d96e8cd602e17711240ac5d117
3ay <lefrost.volkoff@yandex.ru>
2023-05-10 19:59:29 +0300

    rewrite client / server

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,16 +1,44 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.InetAddress;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Client {
-    private static final int myPort = 7777;
-    private static final String hostName = "netology.homework";
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("netology.homework", 12345);
 
-    public static void main(String[] args) throws IOException {
-        Connection connection = new Connection();
-        connection.connectionToSite(hostName, myPort);
-        InetAddress inetAddress = InetAddress.getByName(hostName);
-        System.out.println(hostName + ", ip address: " + inetAddress.getHostAddress());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            String serverMessage = in.readLine();
+            System.out.println("Server: " + serverMessage);
+
+            BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String name = userInputReader.readLine();
+            out.println(name);
+
+            serverMessage = in.readLine();
+            System.out.println("Server: " + serverMessage);
+
+            String answer = userInputReader.readLine();
+            out.println(answer);
+
+            serverMessage = in.readLine();
+            System.out.println("Server: " + serverMessage);
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+
+
+
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,38 +1,39 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        int myPort = 7777;
-        Connection connection = new Connection();
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("Server started. Waiting for a client...");
 
-        while (true) {
-            Socket socket = connection.runServerByPort(myPort);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
             out.println("Write your name:");
-            final String name = reader.readLine();
-            out.println(name);
-            out.println(String.format("Hi %s, your port is %d", name, socket.getPort()));
+            String name = in.readLine();
+            System.out.println("Client: " + name);
 
             out.println("Are you a child? (yes/no):");
-            String answer = reader.readLine();
-            out.println("Received answer from client: " + answer);
+            String answer = in.readLine();
+            System.out.println("Client: " + answer);
 
-            String response;
             if (answer.equalsIgnoreCase("yes")) {
-                response = "Welcome to the kids area, " + name + "! Let's play!";
+                out.println("Welcome to the kids area, " + name + "! Let's play!");
             } else {
-                response = "Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!";
+                out.println("Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!");
             }
-            out.println(response);
-            out.println(reader.readLine());
-            socket.close();
+
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
